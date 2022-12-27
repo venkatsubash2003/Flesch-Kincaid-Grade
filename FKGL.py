03a1ad1c8c92e4331f141e60ae16139d08f74b59
venkatsubash2003 <88431249+venkatsubash2003@users.noreply.github.com>
2022-12-27 14:54:29 +0530

    Update FKGL.py
    FKGL code for accurate scores

--- FKGL.py
+++ FKGL.py
@@ -1,111 +1,74 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": []
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "code",
-      "execution_count": 6,
-      "metadata": {
-        "colab": {
-          "base_uri": "https://localhost:8080/"
-        },
-        "id": "QE3tq4ZUCVNi",
-        "outputId": "fc5ad6a5-f5ef-408f-ce0b-9e56ffa8926d"
-      },
-      "outputs": [
-        {
-          "output_type": "stream",
-          "name": "stdout",
-          "text": [
-            "Looking in indexes: https://pypi.org/simple, https://us-python.pkg.dev/colab-wheels/public/simple/\n",
-            "Requirement already satisfied: textstat in /usr/local/lib/python3.8/dist-packages (0.7.3)\n",
-            "Requirement already satisfied: pyphen in /usr/local/lib/python3.8/dist-packages (from textstat) (0.13.2)\n",
-            "Enter a sentence: Plato is the earliest important educational thinker, and education is an essential element in “The Republic” (his most important work on philosophy and political theory, written around 360 B.C.). In it, he advocates some rather extreme methods: removing children from their mothers’ care and raising them as wards of the state, and differentiating children suitable to the various castes, the highest receiving the most education, so that they could act as guardians of the city and care for the less able. He believed that education should be holistic, including facts, skills, physical discipline, music, and art. Plato believed that talent and intelligence are not distributed genetically and thus are be found in children born to all classes, although his proposed system of selective public education for an educated minority of the population does not follow a democratic model.\n",
-            "19.301231884057973\n"
-          ]
-        }
-      ],
-      "source": [
-        "#FKGL\n",
-        "!pip3 install textstat\n",
-        "import textstat\n"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "source": [
-        "sentence = input(\"Enter a sentence: \")\n",
-        "\n",
-        "   \n",
-        "word_count = textstat.lexicon_count(sentence)\n",
-        "sent_count = textstat.sentence_count(sentence)\n",
-        "syllable_count = textstat.syllable_count(sentence)\n",
-        "WordSentRatio = word_count/sent_count\n",
-        "SyllableWordRatio = syllable_count/word_count\n",
-        "grade = (0.39 * WordSentRatio) + (11.8 * SyllableWordRatio) - 15.59\n",
-        "score = 206.835 - 1.015 * (WordSentRatio) - 84.6 * (SyllableWordRatio)\n",
-        "grade = round(grade)\n",
-        "score = round(score)\n",
-        "print(grade,score)\n",
-        "if grade == 5:\n",
-        "  print(\"Very easy to read\")\n",
-        "elif grade == 6:\n",
-        "  print(\"Easy to read\")\n",
-        "elif grade == 7:\n",
-        "  print(\"Fairly easy to read\")\n",
-        "elif grade == 8 or grade == 9:\n",
-        "  print(\"Plain English\")\n",
-        "elif grade == 10 or grade == 11 or grade == 12:\n",
-        "  print(\"Fairly difficult to read\")\n",
-        "elif grade == 13 or grade == 14 or grade == 15:\n",
-        "  print(\"Difficult to read\")\n",
-        "elif grade == 16 or grade == 17 or grade == 18:\n",
-        "  print(\"Very difficult to read\")\n",
-        "elif grade > 18:\n",
-        "  print(\"Too much of difficulty\")\n",
-        "else:\n",
-        "  print(\"Too easy to read\")"
-      ],
-      "metadata": {
-        "colab": {
-          "base_uri": "https://localhost:8080/"
-        },
-        "id": "TTZEPxp5FpQV",
-        "outputId": "a69c4704-b6d4-42a8-cb97-92a90f53e341"
-      },
-      "execution_count": 72,
-      "outputs": [
-        {
-          "output_type": "stream",
-          "name": "stdout",
-          "text": [
-            "Enter a sentence: Philosophy of Education is a label applied to the study of the purpose, process, nature and ideals of education. It can be considered a branch of both philosophy and education. Education can be defined as the teaching and learning of specific skills, and the imparting of knowledge, judgment and wisdom, and is something broader than the societal institution of education we often speak of.\n",
-            "13 41\n",
-            "Difficult to read\n"
-          ]
-        }
-      ]
-    },
-    {
-      "cell_type": "code",
-      "source": [],
-      "metadata": {
-        "id": "3cuvLkqQZ8V1"
-      },
-      "execution_count": null,
-      "outputs": []
-    }
-  ]
-}
+import textstat
+
+#taking a input paragraph
+sentence = input("Enter a sentence: ")
+
+#calculating number of words,sentences and syllables
+word_count = textstat.lexicon_count(sentence)
+sent_count = textstat.sentence_count(sentence)
+syllable_count = textstat.syllable_count(sentence)
+
+#calculating words per sentence Ratio
+WordSentRatio = word_count/sent_count
+
+#calculating Syllable per word Ratio
+SyllableWordRatio = syllable_count/word_count
+
+#calculating Flesch Kincaid Grade level
+grade = (0.39 * WordSentRatio) + (11.8 * SyllableWordRatio) - 15.59
+
+#calculating FLesch ease score
+score = 206.835 - 1.015 * (WordSentRatio) - 84.6 * (SyllableWordRatio)
+
+#printing Flesch Grade and Flesch Score
+print("Flesch Kincaid Grade:",round(grade,2),"Flesch Ease Score:",round(score,2))
+
+#rounding the grade and score to approximate integer values
+grade = round(grade)
+score = round(score)
+
+#classifying sentences into different difficulty levels based on scores and grades
+if 90 <= score <= 100:
+  print("Level of Difficulty->Very easy to read")
+  print("Rounded Grade is:",grade)
+  print("Can be read by 5th grade students")
+elif 80 <= score <= 90:
+  print("Level of Difficulty->Easy to read")
+  print("Rounded Grade is:",grade)
+  print("Can be read by 6th grade students")
+elif 70 <= score <= 80:
+  print("Level of Difficulty->Fairly easy to read")
+  print("Rounded Grade is:",grade)
+  print("Can be read by 7th grade students")
+elif 60 <= score <= 70:
+  print("Level of Difficulty->Plain English")
+  print("Rounded Grade is:",grade)
+  print("Can be read by 8th and 9th grade students")
+elif 50 <= score <= 60:
+  print("Level of Difficulty->Fairly difficult to read")
+  print("Rounded Grade is:",grade)
+  if grade <= 10:
+    print("Can be read by 10th,11th and 12th grade students")
+  else:
+    print("Can be read by College Students")
+elif 30 <= score <= 50:
+  print("Level of Difficulty->Difficult to read")
+  print("Rounded Grade is:",grade)
+  print("Can be read by College students")
+elif 0 <= score <= 30:
+  print("Level of Difficulty->Very difficult to read")
+  print("Rounded Grade is:",grade)
+  if grade < 16:
+    print("Can be read by College students")
+  else:
+    print("Can be read by College Graduates")
+elif score < 0:
+  print("Level of Difficulty->Peak Difficulty level")
+else:
+  print("Level of Difficulty->Too easy to read.Even a small child can read it.")
+  print("Rounded Grade is:",grade)
+  print("Can be read by College Graduate")
+print("Total words:",word_count)
+print("Total Sentences:",sent_count)
+print("Average Syllables per word:",round(SyllableWordRatio,2))
+print("Average Words per Sentence:",round(WordSentRatio,2))
